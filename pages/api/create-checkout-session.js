4c40413cddcb91f4f6f6d73c00ea195bdb84cffa
monmarthev <victor.monmarthe@icloud.com>
2025-04-16 19:05:19 +0200

    styles

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,5 +1,6 @@
 // pages/api/create-checkout-session.js
 import Stripe from "stripe";
+import "../../styles/memphis.css";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
